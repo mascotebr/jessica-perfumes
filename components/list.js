7fe999812ed0814a1b58ca64ed2ac40e539fe91e
Mascotebr <ga_con@hotmail.com>
2025-10-07 11:56:27 -0300

    adicionado motion list

--- components/list.js
+++ components/list.js
@@ -1,57 +1,42 @@
 import Link from "next/link";
-
+import { motion } from "motion/react";
+import { listaFemininos, listaMasculinos } from "./data";
 export default function List() {
   const wame =
     "https://wa.me/5515996948425?text=Olá, me interresei pelo perfume ";
-  const listaMasculinos = [
-    {
-      nome: "LaurPolo Green Ent",
-      foto: "/laurPoloGreenent.png",
-      preco: 120.0,
-    },
-    {
-      nome: "Sauvage",
-      foto: "/sauvage.png",
-      preco: 120.0,
-    },
-    {
-      nome: "Invictus",
-      foto: "/invictus.webp",
-      preco: 120.0,
-    },
-    {
-      nome: "Scandal",
-      foto: "/scandal.webp",
-      preco: 120.0,
-    },
-    {
-      nome: "Creed Advento",
-      foto: "/creedadvento.webp",
-      preco: 120.0,
-    },
-    {
-      nome: "Bleu de chanel",
-      foto: "/bleuchanel.jpg",
-      preco: 120.0,
-    },
-  ];
 
-  const listaFemininos = [
-    {
-      nome: "La vie est belle ",
-      foto: "/lavieestbelle.webp",
-      preco: 120.0,
+  const containerVariants = {
+    visible: {
+      transition: {
+        staggerChildren: 0.075,
+      },
     },
-    {
-      nome: "La nuit tresor",
-      foto: "/lanuittresor.jpg",
-      preco: 120.0,
-    },
-  ];
+  };
 
-  const card = (item) => {
+  const itemVariants = {
+    hidden: { opacity: 0, y: 32 },
+    visible: (i) => {
+      const baseDelay = i * 0.25;
+
+      return {
+        opacity: 1,
+        x: 0,
+        y: 0,
+        transition: {
+          duration: 1,
+          delay: baseDelay,
+        },
+      };
+    },
+  };
+  const card = (item, index) => {
     return (
-      <div key={item.nome} className="card">
+      <motion.div
+        key={item.nome}
+        className="card"
+        variants={itemVariants}
+        custom={index}
+      >
         <Link href={`${wame}${item.nome}`}>
           <div>
             <img src={item.foto} alt={item.nome} />
@@ -59,15 +44,29 @@ export default function List() {
           </div>
           <div className="p">R${item.preco}</div>
         </Link>
-      </div>
+      </motion.div>
     );
   };
   return (
-    <>
+    <section>
       <h1 className="blue">Masculinos</h1>
-      <section className="list">{listaMasculinos.map(card)}</section>
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={containerVariants}
+        className="list"
+      >
+        {listaMasculinos.map(card)}
+      </motion.div>
       <h1 className="pink">Femininos</h1>
-      <section className="list">{listaFemininos.map(card)}</section>
-    </>
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={containerVariants}
+        className="list"
+      >
+        {listaFemininos.map(card)}
+      </motion.div>
+    </section>
   );
 }
