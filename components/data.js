67146557cb05721d627cea840d35ecd17b946035
Mascotebr <ga_con@hotmail.com>
2025-10-07 11:56:45 -0300

    adicionado novos perfumes e imagens data

---
+++ components/data.js
@@ -0,0 +1,100 @@
+export const listaMasculinos = [
+  {
+    nome: "LaurPolo Green Ent",
+    foto: "/laurPoloGreenent.png",
+    preco: 120.0,
+  },
+  {
+    nome: "Sauvage",
+    foto: "/sauvage.png",
+    preco: 120.0,
+  },
+  {
+    nome: "Invictus",
+    foto: "/invictus.webp",
+    preco: 120.0,
+  },
+  {
+    nome: "Scandal",
+    foto: "/scandal.webp",
+    preco: 120.0,
+  },
+  {
+    nome: "Creed Advento",
+    foto: "/creedadvento.webp",
+    preco: 120.0,
+  },
+  {
+    nome: "Bleu de chanel",
+    foto: "/bleuchanel.jpg",
+    preco: 120.0,
+  },
+  {
+    nome: "Prada Milano",
+    foto: "/pradamilano.webp",
+    preco: 120.0,
+  },
+  {
+    nome: "Ferrari Red",
+    foto: "/ferrarired.webp",
+    preco: 120.0,
+  },
+  {
+    nome: "Bad Boy",
+    foto: "/badboy.webp",
+    preco: 120.0,
+  },
+  {
+    nome: "Assad",
+    foto: "/assad.jpg",
+    preco: 120.0,
+  },
+];
+
+export const listaFemininos = [
+  {
+    nome: "La Vie Est Belle",
+    foto: "/lavieestbelle.webp",
+    preco: 120.0,
+  },
+  {
+    nome: "La Nuit Tresor",
+    foto: "/lanuittresor.jpg",
+    preco: 120.0,
+  },
+  {
+    nome: "La nuit tresor",
+    foto: "/lanuittresor.jpg",
+    preco: 120.0,
+  },
+  {
+    nome: "Scandel",
+    foto: "/scandel.jpg",
+    preco: 120.0,
+  },
+  {
+    nome: "Cyloé",
+    foto: "/cyloe.webp",
+    preco: 120.0,
+  },
+  {
+    nome: "Light Blue Dolce",
+    foto: "/dolcecabanalight.webp",
+    preco: 120.0,
+  },
+  {
+    nome: "Chanel5",
+    foto: "/chanel5.jpg",
+    preco: 120.0,
+  },
+  {
+    nome: "Jadore",
+    foto: "/jadore.jpg",
+    preco: 120.0,
+  },
+  {
+    nome: "Love love",
+    foto: "/lovelove.webp",
+    preco: 120.0,
+  },
+];
